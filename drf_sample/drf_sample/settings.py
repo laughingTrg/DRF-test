8cc6516e0252c4f11cda59baec23a29e5afcaac5
laughing <laughinChrist@gmail.com>
2023-09-27 20:35:50 +0500

    add djoser and configure

--- drf_sample/drf_sample/settings.py
+++ drf_sample/drf_sample/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'tonus',
     'drf_yasg',
     'rest_framework.authtoken',
+    'djoser',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -143,6 +145,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
